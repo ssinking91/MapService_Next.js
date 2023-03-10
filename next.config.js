04a08ecf4801eae660425a2dbd4aa519ed52d4ce
신항민 <tlsgkdals71@naver.com>
2023-03-10 13:54:51 +0900

    Feat : map_service UI

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+// const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['inflearn-nextjs.vercel.app', 'search.pstatic.net'],
+  },
+  i18n: {
+    /** https://nextjs.org/docs/advanced-features/i18n-routing#getting-started */
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  },
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  //   prependData: `@import "src/styles/header.module.scss"; @import "styles/globals.scss"; @import "src/styles/detail.module.scss @import "src/styles/feedback.module.scss @import "src/styles/map.module.scss`,
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
+import Script from 'next/script';
+import SEO from '../../seo.config';
+import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/** SEO */}
+      <DefaultSeo {...SEO} />
+      {/** GA https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-PMBX725CN0`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PMBX725CN0');
+          `,
+        }}
+      />
+      {/** Component */}
+      <Component {...pageProps} />
+    </>
+  );
 }
