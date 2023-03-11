a8d1c63bc6676ef91af117d41ec0362086f41857
신항민 <tlsgkdals71@naver.com>
2023-03-12 03:45:16 +0900

    Feat : DetailSection UI

--- src/components/home/DetailContent.tsx
+++ src/components/home/DetailContent.tsx
@@ -15,6 +15,7 @@ const DetailContent = ({ currentStore, expanded }: Props) => {
     <div
       className={`${styles.detailContent} ${expanded ? styles.expanded : ''}`}
     >
+      {/* 이미지 영역 */}
       <div className={styles.images}>
         {currentStore.images.slice(0, 3).map((image) => (
           <div
@@ -22,12 +23,15 @@ const DetailContent = ({ currentStore, expanded }: Props) => {
             key={image}
           >
             <Image
+              // src는 네이버 외부링크이므로 직접 data:image로 주었음
               src={image}
               alt=""
               fill
               style={{ objectFit: 'cover' }}
               sizes="120px"
+              // 정적 import라면 placeholder="blur"만 추가
               placeholder="blur"
+              // 동적 import(외부 경로에서 이미지를 받아와야되는 상황)라면 blurDataURL = 'data:image/gif;base64, + base64로 인코딩된 이미지 픽셀'까지 추가
               blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO0WhFsDwADzwF2mLYSJgAAAABJRU5ErkJggg=="
               priority
             />
@@ -36,11 +40,13 @@ const DetailContent = ({ currentStore, expanded }: Props) => {
       </div>
       {expanded && (
         <>
+          {/* 설명 영역 */}
           <div className={styles.description}>
             <h2>설명</h2>
             <p>{currentStore.description}</p>
           </div>
           <hr />
+          {/* 기본 정보 영역 */}
           <div className={styles.basicInfo}>
             <h2>기본 정보</h2>
             <div className="address">
@@ -52,6 +58,7 @@ const DetailContent = ({ currentStore, expanded }: Props) => {
               <span>{currentStore.phone || '정보가 없습니다.'}</span>
             </div>
             <div className="naverUrl">
+              {/* static하게 import한 파일이어서 width와 height를 바로 지정함 */}
               <Image src={Naver} width={20} height={20} alt="" />
               <a
                 href={`https://pcmap.place.naver.com/restaurant/${currentStore.nid}/home`}
@@ -63,6 +70,7 @@ const DetailContent = ({ currentStore, expanded }: Props) => {
             </div>
           </div>
           <hr />
+          {/* 메뉴 영역 */}
           <div className={styles.menus}>
             <h2>메뉴</h2>
             <ul>
